5beeab93f79652132fea96e31040a7890afa9df3
YeshNels <paleojosh@sbcglobal.net>
2022-09-16 14:58:46 -0700

    Adds the bare skeleton of the models, and the beginnings of the main server file. Still no controllers or views as of yet.

---
+++ server.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
